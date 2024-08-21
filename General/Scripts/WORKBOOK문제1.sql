b106fa5d3921246eb65deabd76422e65eca326c9
ykhong1107 <nada_yk@naver.com>
2024-08-21 17:26:11 +0900

    24.08.21 서브쿼리나머지, DML(INSERT, UPDATE, DELETE)

--- General/Scripts/WORKBOOK문제1.sql
+++ General/Scripts/WORKBOOK문제1.sql
@@ -298,18 +298,91 @@ ORDER BY
 -- 단, 출력헤더에는 "학생이름", "학번", "거주지 주소"가 출력되도록
 SELECT
 	STUDENT_NAME 학생이름,
-	SUBSTR(STUDENT_SSN, 1, 6) 학번,
+	SUBSTR(STUDENT_NO, 1, 6) 학번,
 	STUDENT_ADDRESS "거주지 주소"
 FROM
 	TB_STUDENT
+WHERE
+	 STUDENT_ADDRESS LIKE '%강원도%'
+OR STUDENT_ADDRESS LIKE '%경기도%'
+AND SUBSTR(STUDENT_NO, 1, 1) = '9'
+ORDER BY 1 ASC;
+
+-- 4번 
+-- 현재 법학과 교수의 이름, 주민등록 번호를 나이가 많은 순서부터 조회하시오.
+SELECT
+	PROFESSOR_NAME,
+	PROFESSOR_SSN
+FROM 
+	TB_PROFESSOR
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+WHERE
+	DEPARTMENT_NAME = '법학과'
+ORDER BY 2;
+
+-- 5번 
+-- 2004년 2학기에 'C3118100' 과목을 수강한 학생들의 학점을 조회하려고 한다.  
+-- 학점이 높은 학생부터 표시하고, 학점이 같으면 학번이 낮은 학생부터 조회하시오. 
+-- (참고) 소수점 아래 2자리까지 0으로 표현 : TO_CHAR(NUMBER, 'FM9.00') 
+-- (FM : 조회 결과 양쪽 공백 제거)
+SELECT
+	STUDENT_NO,
+	TO_CHAR(POINT, 'FM9.00')
+FROM TB_GRADE
+WHERE TERM_NO = '200402' AND CLASS_NO = 'C3118100'
+ORDER BY POINT DESC;
+
+-- 6번 
+-- 학생 번호, 학생 이름, 학과 이름을 학생 이름 오름차순으로 조회하시오. 
+SELECT
+	STUDENT_NO,
+	STUDENT_NAME,
+	DEPARTMENT_NAME
+FROM TB_STUDENT
+JOIN TB_DEPARTMENT USING (DEPARTMENT_NO)
+ORDER BY 2;
+
+-- 7번 
+-- 춘 기술대학교의 과목 이름, 해당 과목을 수업하는 학과 이름을 조회하시오.
+SELECT
+	CLASS_NAME,
+	DEPARTMENT_NAME
+FROM TB_DEPARTMENT
+JOIN TB_CLASS USING(DEPARTMENT_NO);
+
+-- 8번 
+-- 과목, 해당 과목 교수 이름을 조회하시오.
+SELECT
+   CLASS_NAME,
+   PROFESSOR_NAME
+FROM
+   TB_CLASS
+JOIN
+   TB_CLASS_PROFESSOR USING(CLASS_NO)
+JOIN
+   TB_PROFESSOR USING(PROFESSOR_NO)
+ORDER BY
+   PROFESSOR_NAME ASC;
 
 
+-- 9번 
+-- 8번의 결과 중 '인문 사회' 계열에 속한  
+-- 과목명, 교수이름을 과목명 오름차순으로 조회하시오.
+
+	
+-- 10번 
+-- 음악학과 학생들의 "학번", "학생 이름", "전체 평점"을 조회하시오. 
+-- (단, 평점은 소수점 1자리까지만 반올림하여 표시한다.) 
 
 
+-- 11번 
+-- 학번이 A313047인 학생의 학과이름, 학생이름, 지도교수 이름을 조회하시오.
+	
+	
+-- 12번 
+-- 2007년도에 '인간관계론' 과목을 수강한 학생을 찾아  
+-- 학생이름과 수강학기를 조회하는 SQL을 작성하시오.
 
-
-
-
-
-
-
+-- 13번 
+-- 예체능 계열 과목 중 과목 담당교수를 한 명도 배정받지 못한 과목을 찾아  
+-- 과목 이름, 학과 이름을 조회하시오.
