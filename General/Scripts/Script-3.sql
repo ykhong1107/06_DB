b4377fba0f8425c4ff25a58d168f6f35c15acb15
ykhong1107 <nada_yk@naver.com>
2024-10-15 19:06:30 +0900

    24.10.15 DB

--- General/Scripts/Script-3.sql
+++ General/Scripts/Script-3.sql
@@ -715,7 +715,36 @@ AND (BOARD_TITLE LIKE '%' || '11' || '%'
 		OR  BOARD_CONTENT LIKE '%' || '11' || '%')
 
 -- 작성자 검색
-AND MEMBER_NICKNAME LIKE '%' || '11' || '%'
+AND MEMBER_NICKNAME LIKE '%' || '11' || '%';
+
+
+
+--------------------------------------------------------------------
+
+
+/* 현재 게시글이 속해있는 페이지 번호 조회 */
+SELECT RNUM, BOARD_NO, CEIL(RNUM/10) CP
+FROM
+	(SELECT 
+		ROW_NUMBER() OVER(ORDER BY BOARD_NO DESC) RNUM
+		,BOARD_NO
+	FROM "BOARD"
+	JOIN "MEMBER" USING(MEMBER_NO)
+	WHERE BOARD_CODE = 1
+	AND   BOARD_DEL_FL = 'N'
+	
+	--AND   BOARD_TITLE LIKE '%' || '11' || '%'
+	--AND   BOARD_CONTENT LIKE '%' || '11' || '%'
+--	AND   
+--		(BOARD_TITLE LIKE '%' || '11' || '%'
+--		 OR 
+--		 BOARD_CONTENT LIKE '%' || '11' || '%')
+
+	AND MEMBER_NICKNAME LIKE '%' || '수' || '%'
+	)
+WHERE BOARD_NO = 1154;
+
+
 
 
 
